716602463dde9f3399029d6aaaed741fe7580503
LennertDefauw <lennert.defauw@jimber.org>
2022-07-06 18:37:03 +0200

    Fix backend crash

--- apps/api/start.sh
+++ apps/api/start.sh
@@ -1,2 +1,2 @@
-./wait-for-it.sh ${DATABASE_HOST:-timby_db}:${DATABASE_PORT:-3306} --strict --timeout=45  -- yarn run typeorm:migrate-prod
-pm2-runtime start ./dist/main.js &
+./wait-for-it.sh "${DATABASE_HOST:-project_db}":"${DATABASE_PORT:-3306}" --strict --timeout=45 -- npm run prisma:migrate-prod && npm run prisma:seed
+pm2-runtime start ./dist/src/main.js
