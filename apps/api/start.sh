628b8bdb7aed048dcb062f0c77c0f1091f182216
LennertDefauw <lennert.defauw@jimber.org>
2022-10-20 10:06:50 +0200

    Fixes for helm charts

--- apps/api/start.sh
+++ apps/api/start.sh
@@ -1,2 +1,2 @@
-./wait-for-it.sh "${DATABASE_HOST:-project_db}":"${DATABASE_PORT:-3306}" --strict --timeout=45 -- npm run prisma:migrate-prod
+./wait-for-it.sh "${DATABASE_HOST:-dev-database}":"${DATABASE_PORT:-3306}" --strict --timeout=45 -- npm run prisma:migrate-prod
 pm2-runtime start ./dist/main.js
