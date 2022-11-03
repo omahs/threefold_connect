291e6f36a5e52ccbc81028a793d4007a6e1af36e
LennertDefauw <lennert.defauw@jimber.org>
2022-11-03 12:42:36 +0100

    feat: landing page

--- mobile/lib/views/home/home.view.dart
+++ mobile/lib/views/home/home.view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:threebotlogin/core/components/dividers/box.dividers.dart';
 import 'package:threebotlogin/core/components/tabs/tabs.core.dart';
-import 'package:threebotlogin/views/recover/recover.dialogs.dart';
+import 'package:threebotlogin/core/storage/globals.storage.dart';
+import 'package:threebotlogin/views/connection/connection.widgets.dart';
+import 'package:threebotlogin/views/home/home.widgets.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen();
@@ -13,15 +16,25 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
+    Globals().globalBuildContext = context;
     return LayoutDrawer(
       titleText: 'Home',
       content: Container(
-        child: ElevatedButton(
-          onPressed: () async {
-            await showSuccessfullyRecovered();
-          },
-          child: Text('a'),
-        ),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [logo, kSizedBoxSm, registeredLogo(), kSizedBoxSm, introText]),
+      ),
+    );
+  }
+
+  Widget registeredLogo() {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: MediaQuery.of(context).size.width * 0.5,
+      decoration: BoxDecoration(
+        image: DecorationImage(fit: BoxFit.fill, image: AssetImage('assets/threefold_registered.png')),
       ),
     );
   }
